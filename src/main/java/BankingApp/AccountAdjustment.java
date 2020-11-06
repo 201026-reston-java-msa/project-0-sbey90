69e300160c4f9d963b48468674ba15add1eeb07a
sbey90 <63749496+sbey90@users.noreply.github.com>
2020-11-05 23:07:53 -0500

    Delete AccountAdjustment.java

--- src/main/java/BankingApp/AccountAdjustment.java
+++
@@ -1,9 +0,0 @@
-package BankingApp;
-
-public interface AccountAdjustment {
-	
-	public void view(String customerName);  // implemented in Admin - need to add to Employee class
-	public void edit(); // replace();
-	public void approveAndDeny();
-
-}
